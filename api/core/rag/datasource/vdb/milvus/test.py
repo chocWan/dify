580b84616bd77ba966631a2e78e14946ecd551c7
choc_wan <choc_wan@qq.com>
2025-04-25 09:29:13 +0800

    chore

--- api/core/rag/datasource/vdb/milvus/test.py
+++
@@ -1,4 +0,0 @@
-from pymilvus import MilvusClient, MilvusException  
-
-client = MilvusClient(uri="http://10.184.74.120:19530", user="root", password="1qasw@", db_name="default")
-print(client.get_server_version())
